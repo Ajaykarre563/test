cbdbe77bd556235791be27468f39247ce8b5f8be
Ajay karre <karreajay37@gmail.com>
2025-12-19 01:02:42 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,9 @@
+# Taking input from user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Adding the numbers
+sum = a + b
+
+# Printing the result
+print("Sum is:", sum)
