a5d6b7c1d724dfac9ea80b34e9c0d6be9df21c1b
Ajay karre <karreajay37@gmail.com>
2025-12-23 02:59:42 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,9 +1,9 @@
 # Taking input from user
 a = int(input("Enter first number: "))
-c = int(input("Enter second number: "))
+d = int(input("Enter second number: "))
 
 # Adding the numbers
-sum = a + c
+sum = a + d
 
 # Printing the result
 print("Sum is:", sum)
